bdc0446b977bc15313f2026e9055848aba5c1cc7
Anas Khan <anas.khan@hotmail.co.uk>
2024-02-12 15:02:24 +0000

    Created the feature to add new client into the form

--- MyStore/Pages/Clients/Create.cshtml.cs
+++ MyStore/Pages/Clients/Create.cshtml.cs
@@ -1,31 +1,75 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
 
 namespace MyStore.Pages.Clients
 {
     public class CreateModel : PageModel
     {
         public ClientInfo clientInfo = new ClientInfo();
-        public String errorMessage = "";
+        public String error = "";
+        public String success = "";
         public void OnGet()
         {
         }
 
-        public void onPost()
+        public void OnPost()
         {
+
             clientInfo.name = Request.Form["name"];
             clientInfo.email = Request.Form["email"];
             clientInfo.phone = Request.Form["phone"];
             clientInfo.address = Request.Form["address"];
-
+            
+             
             if (clientInfo.name.Length == 0 || clientInfo.email.Length == 0 ||
                 clientInfo.phone.Length == 0 || clientInfo.address.Length == 0)
             {
-                errorMessage = "All fields are required";
+                error = "All fields are required";
+                return;
+            }
+            
+
+            // Save the client into the database
+            try
+            {
+                String connectionString = "Data Source=.\\sqlexpress;Initial Catalog=mystore;Integrated Security=True;Encrypt=False";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "INSERT INTO clients " +
+                                "(name, email, phone, address) VALUES" +
+                                "(@name, @email, @phone, @address);";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", clientInfo.name);
+                        command.Parameters.AddWithValue("@email", clientInfo.email);
+                        command.Parameters.AddWithValue("@phone", clientInfo.phone);
+                        command.Parameters.AddWithValue("@address", clientInfo.address);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+                
+            catch (Exception ex)
+            {
+                error = ex.Message;
                 return;
             }
 
-            // Save the client into the database
+
+            clientInfo.name = "";
+            clientInfo.email = "";
+            clientInfo.phone = "";
+            clientInfo.address = "";
+            success = "Client addedd successfully!";
+
+            Response.Redirect("/Clients/Index");
         }
     }
 }
