144edabe5e98bb4310731ab45aa754571cc92ad8
Anas Khan <anas.khan@hotmail.co.uk>
2024-02-27 15:09:30 +0000

    Created the database query to edit client information

--- MyStore/Pages/Clients/Edit.cshtml.cs
+++ MyStore/Pages/Clients/Edit.cshtml.cs
@@ -1,12 +1,102 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
 namespace MyStore.Pages.Clients
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
-        {
+
+        public ClientInfo clientInfo = new ClientInfo();
+        public string error = "";
+        public string success = "";
+        
+        public void OnGet() {
+
+            String id = Request.Query["id"];
+
+            try
+            {
+                String connectionString = "Data Source=.\\sqlexpress;Initial Catalog=mystore;Integrated Security=True;Encrypt=False";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT * FROM clients WHERE id=@id";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("id", id);
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                clientInfo.id = "" + reader.GetInt32(0);
+                                clientInfo.name = "" + reader.GetString(1);
+                                clientInfo.email = reader.GetString(2);
+                                clientInfo.phone = reader.GetString(3);
+                                clientInfo.address = reader.GetString(4);
+
+                            }
+                        }
+                    }
+
+                }
+                    
+            }
+            catch (Exception ex)
+            {
+
+                error = ex.Message;
+            }
+        }
+
+        public void OnPost() {
+
+            clientInfo.id = Request.Form["id"];
+            clientInfo.name = Request.Form["name"];
+            clientInfo.email = Request.Form["email"];
+            clientInfo.phone = Request.Form["phone"];
+            clientInfo.address = Request.Form["address"];
+
+
+            if (clientInfo.id.Length == 0 || clientInfo.name.Length == 0 || 
+                clientInfo.email.Length == 0 ||
+                clientInfo.phone.Length == 0 || clientInfo.address.Length == 0)
+            {
+                error = "All fields are required";
+                return;
+            }
+
+            try
+            {
+                String connectionString = "Data Source=.\\sqlexpress;Initial Catalog=mystore;Integrated Security=True;Encrypt=False";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "UPDATE c;ients " +
+                                 "SET name=@name, email=@email, phone=@phone, address=@address " +
+                                 "WHERE id=@id";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                        command.Parameters.AddWithValue("@name", clientInfo.name);
+                        command.Parameters.AddWithValue("@email", clientInfo.email);
+                        command.Parameters.AddWithValue("@phone", clientInfo.phone);
+                        command.Parameters.AddWithValue("@address", clientInfo.address);
+                        command.Parameters.AddWithValue("@id", clientInfo.id);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                error = ex.Message;
+                return;
+            }
+
+            Response.Redirect("/Clients/Index");
         }
     }
 }
